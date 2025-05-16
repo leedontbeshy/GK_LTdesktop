aa333feef645a903101655454838d22262d391d7
lazzerex <nahmsix2005@gmail.com>
2025-05-16 13:14:22 +0700

    database connection

--- GiuaKi_LT_Desktop/Form1.cs
+++ GiuaKi_LT_Desktop/Form1.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.Sqlite;
+//using Microsoft.EntityFrameworkCore;
+
 namespace GiuaKi_LT_Desktop
 {
     public partial class Form1 : Form
@@ -5,6 +8,31 @@ namespace GiuaKi_LT_Desktop
         public Form1()
         {
             InitializeComponent();
+            ConnectToDatabase();
+        }
+
+        private void ConnectToDatabase()
+        {
+            // Path to your SQLite database file
+            var connectionString = "Data Source=your_database_file.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                // Example: create a table if it doesn't exist
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                        CREATE TABLE IF NOT EXISTS SampleTable (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Name TEXT
+                        );
+                    ";
+                command.ExecuteNonQuery();
+
+                // You can now execute queries using this connection
+            }
         }
     }
 }
