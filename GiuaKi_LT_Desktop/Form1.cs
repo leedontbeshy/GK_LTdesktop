dca746656fe8145906cd376f02cb92c829380ebb
lazzerex <nahmsix2005@gmail.com>
2025-05-16 15:12:05 +0700

    move dulieu.txt

--- GiuaKi_LT_Desktop/Form1.cs
+++ GiuaKi_LT_Desktop/Form1.cs
@@ -6,7 +6,8 @@ namespace GiuaKi_LT_Desktop
 {
     public partial class Form1 : Form
     {
-        private string filePath = @"C:\Users\Administrator\source\repos\GK_LTdesktop1\GiuaKi_LT_Desktop\dulieu.txt";
+        private string filePath = Path.Combine(Application.StartupPath, "dulieu.txt");
+
 
         private List<NhanVien> danhSachNhanVien = new();
         private bool isAdding = false;
